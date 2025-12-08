79a37d3be31be9fa1d24a76f015c071530df16fb
varsha766 <varshakumari370@gmail.com>
2025-12-08 21:51:39 +0530

    fixed credit  guard

--- src/utils/redis.provider.ts
+++ src/utils/redis.provider.ts
@@ -2,8 +2,8 @@ import Redis from 'ioredis';
 import * as dotenv from 'dotenv';
 dotenv.config();
 export const redisClient = new Redis({
-    host:
-        process.env.REDIS_HOST ||
-        'redis-stack-service.hypermine-development.svc.cluster.local',
-    port: 6379,
+  host:
+    process.env.REDIS_HOST ||
+    'redis-stack-service.hypermine-development.svc.cluster.local',
+  port: 6379,
 });
