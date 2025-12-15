a962a5a2b06e42561df93a90b0d8fd8ac3b7d410
varsha766 <varshakumari370@gmail.com>
2025-12-15 11:26:31 +0530

    fixed empty creditDetail if we have multiple credit

--- src/credit-manager/services/credit-manager.service.ts
+++ src/credit-manager/services/credit-manager.service.ts
@@ -34,12 +34,11 @@ export class CreditService {
     );
     createCreditManagerDto.validityDuration = validityPeriodInDays;
     Logger.debug(`Credit status:${status}`);
-    let grantDetail;
+    const grantDetail = body;
     if (status === 'Active') {
       expiryTime = this.calculateExpiryTime(
         createCreditManagerDto.validityDuration,
       );
-      grantDetail = body;
     }
     const newCreditDetail = {
       ...createCreditManagerDto,
