3c85c8a8a7e1c52d7a02e56ae92487bad39de872
Pratap2018 <pratapmridha@gmail.com>
2026-06-02 12:22:19 +0530

    added logger in edv

--- src/edv/services/edvClientManager.ts
+++ src/edv/services/edvClientManager.ts
@@ -49,38 +49,57 @@ export class EdvClientManger implements IEdvClientManager {
     this.config = new ConfigService();
   }
 
+  private logStart(fn: string, description: string) {
+    const start = performance.now();
+    Logger.log(`Starting ${fn}() - ${description}`, `EdvClientManger.${fn}`);
+    return start;
+  }
+
+  private logEnd(fn: string, start: number) {
+    const elapsed = (performance.now() - start).toFixed(2);
+    Logger.log(`${fn} finished in ${elapsed}ms`, `EdvClientManger.${fn}`);
+  }
+
   async initate(): Promise<IEdvClientManager> {
-    const ed25519 = this.vaultWallet.ed25519Signer;
-    const x25519 = this.vaultWallet.ed25519Signer;
-    const keyAgreementKey = this.vaultWallet.keyAgreementKey;
+    const start = this.logStart(
+      'initate',
+      'initialize EDV client and register the encrypted data vault',
+    );
+    try {
+      const ed25519 = this.vaultWallet.ed25519Signer;
+      const x25519 = this.vaultWallet.ed25519Signer;
+      const keyAgreementKey = this.vaultWallet.keyAgreementKey;
 
-    this.recipient = [
-      {
-        ...keyAgreementKey,
-        publicKeyMultibase: x25519.publicKeyMultibase,
-      },
-    ];
+      this.recipient = [
+        {
+          ...keyAgreementKey,
+          publicKeyMultibase: x25519.publicKeyMultibase,
+        },
+      ];
 
-    const EDV_BASE_URL = this.config.get('EDV_BASE_URL');
-    Logger.log('EDV_BASE_URL = ' + EDV_BASE_URL, 'edvClientManager');
-    this.vault = new HypersignEdvClientEd25519VerificationKey2020({
-      keyResolver: this.keyResolver,
-      url: EDV_BASE_URL,
-      ed25519VerificationKey2020: ed25519,
-      x25519KeyAgreementKey2020: x25519,
-    });
+      const EDV_BASE_URL = this.config.get('EDV_BASE_URL');
+      Logger.log('EDV_BASE_URL = ' + EDV_BASE_URL, 'edvClientManager');
+      this.vault = new HypersignEdvClientEd25519VerificationKey2020({
+        keyResolver: this.keyResolver,
+        url: EDV_BASE_URL,
+        ed25519VerificationKey2020: ed25519,
+        x25519KeyAgreementKey2020: x25519,
+      });
 
-    const config = {
-      url: EDV_BASE_URL,
-      keyAgreementKey,
-      controller: this.vaultWallet.authenticationKey.id,
-      edvId: this.edvId
-        ? this.edvId
-        : 'urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66',
-    };
+      const config = {
+        url: EDV_BASE_URL,
+        keyAgreementKey,
+        controller: this.vaultWallet.authenticationKey.id,
+        edvId: this.edvId
+          ? this.edvId
+          : 'urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66',
+      };
 
-    const res = await this.vault.registerEdv(config);
-    return this;
+      await this.vault.registerEdv(config);
+      return this;
+    } finally {
+      this.logEnd('initate', start);
+    }
   }
 
   prepareEdvDocument(
@@ -88,30 +107,46 @@ export class EdvClientManger implements IEdvClientManager {
     indexes: Array<{ index: string; unique: boolean }>,
     recipients?: Array<IEncryptionRecipents>,
   ): EDVDocType {
-    const document: any = {
-      document: { content },
-      edvId: this.edvId,
-      indexs: indexes,
-      recipients: recipients ? recipients : this.recipient,
-    };
-    return document;
+    const start = this.logStart(
+      'prepareEdvDocument',
+      'prepare encrypted data vault document payload',
+    );
+    try {
+      const document: any = {
+        document: { content },
+        edvId: this.edvId,
+        indexs: indexes,
+        recipients: recipients ? recipients : this.recipient,
+      };
+      return document;
+    } finally {
+      this.logEnd('prepareEdvDocument', start);
+    }
   }
 
   async insertDocument(doc: EDVDocType): Promise<{ id: string }> {
-    if (doc['recipients'] && doc['recipients'].length !== 0) {
-      doc['recipients'].push(this.recipient[0]);
-    } else {
-      doc['recipients'] = this.recipient;
-    }
-    const resp: IResponse = await this.vault.insertDoc({ ...doc });
+    const start = this.logStart(
+      'insertDocument',
+      'insert an encrypted document into the EDV',
+    );
+    try {
+      if (doc['recipients'] && doc['recipients'].length !== 0) {
+        doc['recipients'].push(this.recipient[0]);
+      } else {
+        doc['recipients'] = this.recipient;
+      }
+      const resp: IResponse = await this.vault.insertDoc({ ...doc });
 
-    if (!resp || !resp.document?.id) {
-      Logger.error(JSON.stringify(resp), 'edvClientManager');
-      throw new Error('Could not insert document');
+      if (!resp || !resp.document?.id) {
+        Logger.error(JSON.stringify(resp), 'edvClientManager');
+        throw new Error('Could not insert document');
+      }
+      return {
+        id: resp.document.id,
+      };
+    } finally {
+      this.logEnd('insertDocument', start);
     }
-    return {
-      id: resp.document.id,
-    };
   }
 
   updateDocument(): any {
@@ -122,41 +157,57 @@ export class EdvClientManger implements IEdvClientManager {
   }
 
   async getDocument(id: string): Promise<IResponse> {
-    Logger.log(
-      'getDocument() method: starts, fetching docs from edvClient',
-      'EdvService',
+    const start = this.logStart(
+      'getDocument',
+      'fetch encrypted document from EDV client',
     );
-    const resp: IResponse = await this.vault.fetchDoc({
-      edvId: this.edvId,
-      documentId: id,
-    });
-    return resp;
+    try {
+      Logger.log(
+        'getDocument() method: starts, fetching docs from edvClient',
+        'EdvService',
+      );
+      const resp: IResponse = await this.vault.fetchDoc({
+        edvId: this.edvId,
+        documentId: id,
+      });
+      return resp;
+    } finally {
+      this.logEnd('getDocument', start);
+    }
   }
 
   async getDecryptedDocument(id: string): Promise<any> {
-    Logger.log('getDecryptedDocument() method: starts....', 'EdvService');
-
-    Logger.log(
-      'getDecryptedDocument() method: fetching doc from edvCLient',
-      'EdvService',
+    const start = this.logStart(
+      'getDecryptedDocument',
+      'fetch and decrypt a document from the EDV',
     );
+    try {
+      Logger.log('getDecryptedDocument() method: starts....', 'EdvService');
 
-    const doc: IResponse = await this.getDocument(id);
-    if (!doc.document) {
-      throw new Error(doc.message);
+      Logger.log(
+        'getDecryptedDocument() method: fetching doc from edvCLient',
+        'EdvService',
+      );
+
+      const doc: IResponse = await this.getDocument(id);
+      if (!doc.document) {
+        throw new Error(doc.message);
+      }
+
+      Logger.log(
+        'getDecryptedDocument() method: decrypting doc using edvClient',
+        'EdvService',
+      );
+
+      const { content } = await this.vault.decryptObject({
+        keyAgreementKey: this.vaultWallet.x25519Signer,
+        jwe: doc.document.jwe,
+      });
+      Logger.log('getDecryptedDocument() method: ends....', 'EdvService');
+
+      return content;
+    } finally {
+      this.logEnd('getDecryptedDocument', start);
     }
-
-    Logger.log(
-      'getDecryptedDocument() method: decrypting doc using edvClient',
-      'EdvService',
-    );
-
-    const { content } = await this.vault.decryptObject({
-      keyAgreementKey: this.vaultWallet.x25519Signer,
-      jwe: doc.document.jwe,
-    });
-    Logger.log('getDecryptedDocument() method: ends....', 'EdvService');
-
-    return content;
   }
 }
--- src/edv/services/vaultWalletManager.ts
+++ src/edv/services/vaultWalletManager.ts
@@ -27,60 +27,34 @@ export class VaultWallet {
         : mnemonic;
   }
 
+  private logStart(fn: string, description: string) {
+    const start = performance.now();
+    Logger.log(`Starting ${fn}() - ${description}`, `VaultWallet.${fn}`);
+    return start;
+  }
+
+  private logEnd(fn: string, start: number) {
+    const elapsed = (performance.now() - start).toFixed(2);
+    Logger.log(`${fn} finished in ${elapsed}ms`, `VaultWallet.${fn}`);
+  }
+
   async Initialize() {
-    const seedEntropy = Bip39.decode(this.mnemonic);
-
-    this.keys = await globalThis.hsSSIdkInstance.did.generateKeys({
-      seed: seedEntropy,
-    });
-
-    this.didDocument = await globalThis.hsSSIdkInstance.did.generate({
-      publicKeyMultibase: this.keys.publicKeyMultibase,
-    });
-
-    this.authenticationKey = {
-      '@context': 'https://w3id.org/security/suites/ed25519-2020/v1',
-      id:
-        this.didDocument.id.split('#')[0] + '#' + this.keys.publicKeyMultibase,
-      controller: this.didDocument.id,
-      publicKeyMultibase: this.keys.publicKeyMultibase,
-      privateKeyMultibase: this.keys.privateKeyMultibase,
-    };
-
-    this.ed25519Signer = await Ed25519VerificationKey2020.from(
-      this.authenticationKey,
+    const start = this.logStart(
+      'Initialize',
+      'derive vault wallet keys and DID document from mnemonic',
     );
+    try {
+      const seedEntropy = Bip39.decode(this.mnemonic);
 
-    this.x25519Signer =
-      await X25519KeyAgreementKey2020.fromEd25519VerificationKey2020({
-        keyPair: {
-          publicKeyMultibase: this.keys.publicKeyMultibase,
-          privateKeyMultibase: this.keys.privateKeyMultibase,
-        },
+      this.keys = await globalThis.hsSSIdkInstance.did.generateKeys({
+        seed: seedEntropy,
       });
 
-    this.x25519Signer.id =
-      this.didDocument.id.split('#')[0] +
-      '#' +
-      this.x25519Signer.publicKeyMultibase;
+      this.didDocument = await globalThis.hsSSIdkInstance.did.generate({
+        publicKeyMultibase: this.keys.publicKeyMultibase,
+      });
 
-    // TODO: confued between x25519Signer & keyAgreementKey
-    this.keyAgreementKey = {
-      id:
-        this.didDocument.id.split('#')[0] +
-        '#' +
-        this.x25519Signer.publicKeyMultibase,
-      type: 'X25519KeyAgreementKey2020',
-      publicKeyMultibase: this.x25519Signer.publicKeyMultibase,
-      privateKeyMultibase: this.x25519Signer.privateKeyMultibase,
-    };
-
-    this.keyResolver = async ({ id }: { id: string }) => {
-      // Resolve the key from the DID Document or from the blockchain or from any other source
-      // sample authentication key after did resolution
-      // Caution: This is just a sample snippet (This will cause error). You should resolve the key from the DID Document or from the blockchain or from any other source
-
-      const authenticationKey = {
+      this.authenticationKey = {
         '@context': 'https://w3id.org/security/suites/ed25519-2020/v1',
         id:
           this.didDocument.id.split('#')[0] +
@@ -88,18 +62,86 @@ export class VaultWallet {
           this.keys.publicKeyMultibase,
         controller: this.didDocument.id,
         publicKeyMultibase: this.keys.publicKeyMultibase,
+        privateKeyMultibase: this.keys.privateKeyMultibase,
       };
-      const ed25519 = await Ed25519VerificationKey2020.from(authenticationKey);
-      return ed25519;
-    };
-    return this;
+
+      this.ed25519Signer = await Ed25519VerificationKey2020.from(
+        this.authenticationKey,
+      );
+
+      this.x25519Signer =
+        await X25519KeyAgreementKey2020.fromEd25519VerificationKey2020({
+          keyPair: {
+            publicKeyMultibase: this.keys.publicKeyMultibase,
+            privateKeyMultibase: this.keys.privateKeyMultibase,
+          },
+        });
+
+      this.x25519Signer.id =
+        this.didDocument.id.split('#')[0] +
+        '#' +
+        this.x25519Signer.publicKeyMultibase;
+
+      // TODO: confued between x25519Signer & keyAgreementKey
+      this.keyAgreementKey = {
+        id:
+          this.didDocument.id.split('#')[0] +
+          '#' +
+          this.x25519Signer.publicKeyMultibase,
+        type: 'X25519KeyAgreementKey2020',
+        publicKeyMultibase: this.x25519Signer.publicKeyMultibase,
+        privateKeyMultibase: this.x25519Signer.privateKeyMultibase,
+      };
+
+      this.keyResolver = async ({ id }: { id: string }) => {
+        // Resolve the key from the DID Document or from the blockchain or from any other source
+        // sample authentication key after did resolution
+        // Caution: This is just a sample snippet (This will cause error). You should resolve the key from the DID Document or from the blockchain or from any other source
+
+        const authenticationKey = {
+          '@context': 'https://w3id.org/security/suites/ed25519-2020/v1',
+          id:
+            this.didDocument.id.split('#')[0] +
+            '#' +
+            this.keys.publicKeyMultibase,
+          controller: this.didDocument.id,
+          publicKeyMultibase: this.keys.publicKeyMultibase,
+        };
+        const ed25519 = await Ed25519VerificationKey2020.from(
+          authenticationKey,
+        );
+        return ed25519;
+      };
+      return this;
+    } finally {
+      this.logEnd('Initialize', start);
+    }
   }
 }
 
 export class VaultWalletManager {
+  private static logStart(fn: string, description: string) {
+    const start = performance.now();
+    Logger.log(`Starting ${fn}() - ${description}`, `VaultWalletManager.${fn}`);
+    return start;
+  }
+
+  private static logEnd(fn: string, start: number) {
+    const elapsed = (performance.now() - start).toFixed(2);
+    Logger.log(`${fn} finished in ${elapsed}ms`, `VaultWalletManager.${fn}`);
+  }
+
   static async getWallet(
     mnemonic: EnglishMnemonic | string,
   ): Promise<VaultWallet> {
-    return new VaultWallet(mnemonic).Initialize();
+    const start = this.logStart(
+      'getWallet',
+      'create and initialize a vault wallet',
+    );
+    try {
+      return new VaultWallet(mnemonic).Initialize();
+    } finally {
+      this.logEnd('getWallet', start);
+    }
   }
 }
